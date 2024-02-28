7867b40e5f401050bbc25531335daa6c0c221943
Snow <1311971563@qq.com>
2024-02-28 10:06:41 +0800

    commit

---
+++ src/main/java/com/snow/serverAdmin/SnowServerAdminApplication.java
@@ -0,0 +1,13 @@
+package com.snow.serverAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowServerAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowServerAdminApplication.class, args);
+    }
+
+}
